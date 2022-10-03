f1c5d99da3992505b6c94c94409b5af953c7987e
SUPERGORNYY <supergornyy@gmail.com>
2022-10-03 10:46:45 -0400

    Added couple of checks

--- src/main/java/com/hillel/gornyi/lessons/lesson10/homework10/transpone.java
+++ src/main/java/com/hillel/gornyi/lessons/lesson10/homework10/transpone.java
@@ -2,59 +2,88 @@ package com.hillel.gornyi.lessons.lesson10.homework10;
 
 import java.util.Scanner;
 import java.lang.Math;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class transpone {
-
-//    Транспонуйте матрицю M x N. Розміри M та N задаються з консолі.
-//    Після введення повинні автоматично створюватися два масиви розміром M x N перший та N x M другий.
 
     public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть розміри двомірної матриці M та N");
-        System.out.println("Введіть кількість рядків");
-        int N = scanner.nextInt();
-        System.out.println("Введіть кількість стовбчиків");
-        int M = scanner.nextInt();
+        int M = 0;
+        int N = 0;
+        int counter = 1;
+        System.out.println("Enter parameters of a new matrix MxN");
 
-        // Будуємо матрицю, бо можемо
+        do {
+            System.out.println("Enter rows number (1 to 99)");
+            if (scanner.hasNextInt()) {
+                M = scanner.nextInt();
+                counter++;
+                if (M >= 1 & M < 100) {
+                    System.out.println("Number of rows is " + M);
+                    System.out.println();
+                    System.out.println("------------------------");
+                    System.out.println("Enter columns number (1 to 99)");
+                    if (scanner.hasNextInt()) {
+                        N = scanner.nextInt();
+                        counter++;
+                        if (N >= 1 & N < 100) {
+                            System.out.println("Number of columns is " + N);
+                            System.out.println();
+                            System.out.println("------------------------");
+                        } else {
+                            System.out.println("You have entered incorrect data, restart program");
+                        }
+                    } else {
+                        System.out.println("You have entered incorrect data, restart program");
 
-        int[][] matrix = new int[M][N];
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                matrix[i][j] = ((int)(Math.random()*100));
+                    }
+                } else {
+                    System.out.println("You have entered incorrect data, restart program");
+                    break;
+                }
+            } else {
+                System.out.println("You have entered incorrect data, restart program");
+                break;
             }
-        }
+        } while (counter <= 2);
 
-        System.out.println("Матриця ще не транспонована, терпіння");
+        // We build matrix because we can
+
         System.out.println();
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.printf("%3d", matrix[i][j]);
+        if (M > 0) {
+
+            int[][] mx1 = new int[M][N];
+            int[][] mx2 = new int[N][M];
+
+            for (int i = 0; i < mx1.length; i++) {
+                for (int j = 0; j < mx1[i].length; j++) {
+                    mx1[i][j] = ((int)(Math.random()*100));
+                }
             }
-            System.out.println();
-        }
 
-        // Транспонуємо-транспонуємо та витранспонуємо
-
-        for (int i = 0; i < M; i++) {
-            for (int j = i+1; j < N; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-
-        System.out.println();
-        System.out.println("А от теперь транспонована. Магія поруч.");
-        for (int j = 0; j < N; j++) {
+            System.out.println("MATRIX BEFORE");
             for (int i = 0; i < M; i++) {
-                System.out.printf("%3d", matrix[i][j]);
+                for (int j = 0; j < N; j++) {
+                    System.out.printf("%3d", mx1[i][j]);
+                }
+                System.out.println();
+                System.out.println();
+                System.out.println("------------------------");
             }
-            System.out.println();
+
+        // Transposing-transposing and will be transposed
+
+            if (M > 0) {
+                System.out.println();
+                System.out.println("MATRIX AFTER");
+                for (int i = 0; i < N; i++) {
+                    for (int j = 0; j < M; j++) {
+                        mx2[i][j] = mx1[j][i];
+                        System.out.printf("%3d", mx2[i][j]);
+                    }
+                    System.out.println();
+                }
+            }
         }
     }
-
 }
